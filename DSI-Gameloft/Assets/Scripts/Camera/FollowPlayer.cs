d4a0d21e88e955c29757423100fa00b5302aad4c
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-03-10 09:46:49 +0100

    Adds Cam and Move

---
+++ DSI-Gameloft/Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+    #region Members
+    public Transform mPlayerTrans;
+    Vector3 mDistanceToPlayer;
+    #endregion
+
+    void Start () {
+        mDistanceToPlayer = new Vector3 (0.0f, 8.0f, -6.0f);
+    }
+
+    void Update () {
+        Vector3 newCamPos = mPlayerTrans.transform.position;
+        newCamPos += mDistanceToPlayer;
+        this.transform.position = newCamPos;
+        this.transform.LookAt (mPlayerTrans.position);
+    }
+}
---
+++ DSI-Gameloft/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    #region Members
+    public float mLerpEase = 0.5f;
+    public float mMaxSpeed = 8.0f;
+    public float mAcceleration = 1.0f;
+    public float mDeceleration = 1.0f;
+    float mCurrentSpeed = 0.0f;
+    public float mPseudoEpsilon = 0.01f;
+
+    int mGroundLayerMask;
+    Vector3 mTargetPosition;
+    NavMeshAgent mNavMeshAgent;
+    #endregion
+
+    void Start () {
+        mGroundLayerMask = LayerMask.GetMask ("Ground");
+        mNavMeshAgent = this.GetComponent<NavMeshAgent> ();
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown (0)) {
+            Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast (ray, out hit, Mathf.Infinity, mGroundLayerMask)) {
+                mTargetPosition = hit.point;
+                mTargetPosition.y = 1.083333f;
+                mNavMeshAgent.SetDestination (mTargetPosition);
+            }
+        }
+    }
+}
