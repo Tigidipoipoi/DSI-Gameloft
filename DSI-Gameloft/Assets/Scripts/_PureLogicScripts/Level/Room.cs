fedf80e010b550df919d387c9cf030019927c0dc
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-03-12 19:48:29 +0100

    WIP procedural LD

---
+++ DSI-Gameloft/Assets/Scripts/_PureLogicScripts/Level/Room.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Room {
+    #region Members
+    public List<RoomPart> m_RoomParts;
+    public int m_WalkableRoomPartCount;
+    public const int c_MaxWaklableRoomParts = 9;
+    public bool m_IsReachable;
+    #endregion
+
+    public void GenerateRoom () {
+        if (m_WalkableRoomPartCount > c_MaxWaklableRoomParts) {
+            m_WalkableRoomPartCount = c_MaxWaklableRoomParts;
+        }
+
+        m_RoomParts = new List<RoomPart> ();
+    }
+}
