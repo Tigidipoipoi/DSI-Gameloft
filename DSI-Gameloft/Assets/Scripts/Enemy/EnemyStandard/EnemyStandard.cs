896f6821a31d7cdd280b3266c180a0ebbed93f17
Baptiste <b.billet@rubika-edu.com>
2015-03-12 11:16:07 +0100

    add enemies

--- DSI-Gameloft/Assets/Scripts/Enemy/EnemyStandard/EnemyStandard.cs
+++ DSI-Gameloft/Assets/Scripts/Enemy/EnemyStandard/EnemyStandard.cs
@@ -1,21 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyFou : Enemy_Script
+public class EnemyStandard : Enemy_Script
 {
+
     public IADestination m_Mouvement;
 
     private bool m_IsReady;
 
-    public float m_ShootCooldown;
-
     public float m_DelayBeforeShoot;
-    public float m_DelayAfterShoot;
 
     public GameObject m_PrefabBullet;
     public Transform m_PointForShoot;
 
-    private GameObject bullet;
 
 	// Use this for initialization
 	public override void Start () 
@@ -26,30 +23,20 @@ public class EnemyFou : Enemy_Script
 	
     IEnumerator WaitAndShoot()
     {
-        yield return new WaitForSeconds(m_ShootCooldown);
-
         if (m_IsAwake == true)
         {
             yield return new WaitForSeconds(m_DelayBeforeShoot);
-            m_Mouvement.enabled = false;
-            bullet = Instantiate(m_PrefabBullet, m_PointForShoot.position, this.transform.rotation) as GameObject ;
+
+            if(m_Mouvement.m_IsAtDistance == false)
+            {
+                StopCoroutine(WaitAndShoot());
+            }
+
+            GameObject bullet = Instantiate(m_PrefabBullet, m_PointForShoot.position, this.transform.rotation) as GameObject ;
             bullet.layer = LayerMask.NameToLayer("EnemyBullet");
-
+            m_IsReady = true;
         }
-
-        while(bullet!=null)
-        {
-            m_IsReady = false;
-            yield return new WaitForSeconds(0.1f);
-        }
-        yield return new WaitForSeconds(m_DelayAfterShoot);
-
-        m_IsReady = true;
-        m_Mouvement.enabled = true;
-
-        yield return null;
     }
-
 
 	// Update is called once per frame
 	public override void Update () 
