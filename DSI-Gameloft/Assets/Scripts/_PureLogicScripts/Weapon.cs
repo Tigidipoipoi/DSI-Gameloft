2496ebeb94afd3211872a2199cbc5feb960ee8d8
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-03-12 10:23:13 +0100

    WIP upgrade weapons

--- DSI-Gameloft/Assets/Scripts/_PureLogicScripts/Weapon.cs
+++ DSI-Gameloft/Assets/Scripts/_PureLogicScripts/Weapon.cs
@@ -8,15 +8,17 @@ public class Weapon {
     public float m_FireRate;
     public bool m_IsAlly;
     public bool m_IsHoming;
+    public int m_CurrentLevel;
     #endregion
 
     public Weapon ()
-        : this (1.0f, 0.5f, true, false) { }
+        : this (1.0f, 0.5f, true, false, 0) { }
 
-    public Weapon (float power, float fireRate, bool isAlly, bool isHoming) {
+    public Weapon (float power, float fireRate, bool isAlly, bool isHoming, int level) {
         m_Power = power;
         m_FireRate = fireRate;
         m_IsAlly = isAlly;
-        m_IsHoming= isHoming;
+        m_IsHoming = isHoming;
+        m_CurrentLevel = level;
     }
 }
