11594d3e1179f051578ff318292dd88f4a4e3c54
Baptiste <b.billet@rubika-edu.com>
2015-03-13 16:47:57 +0100

    time and chest

---
+++ DSI-Gameloft/Assets/Scripts/UI/UIPauseScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIPauseScript : MonoBehaviour {
+
+    private bool m_PauseOn;
+
+    public void LaunchPause()
+    {
+        if(m_PauseOn==false)
+        {
+            m_PauseOn = true;
+            Time.timeScale = 0.0F;
+        }
+        else
+        {
+            m_PauseOn = false;
+            Time.timeScale = 1.0F;
+        }
+    }
+
+}
--- DSI-Gameloft/Assets/Scripts/_Managers/UIManager.cs
+++ DSI-Gameloft/Assets/Scripts/_Managers/UIManager.cs
@@ -23,15 +23,77 @@ public class UIManager : MonoBehaviour {
     Transform m_IGPanel;
     // Temp
     Text m_TimerText;
+    //Combo Kill
+    public int m_ComboKill;
+    bool m_ComboKillCompteur;
+    public float m_ComboKillTime;
+    IEnumerator ComboKillCoroutine;
+
+    public int m_ComboChest;
+    bool m_ComboChestCompteur;
+    public float m_ComboChestTime;
+    IEnumerator ComboChestCoroutine;
     #endregion
 
     void Start () {
         m_IGPanel = GameObject.Find ("Canvas").transform.FindChild ("IGPanel");
         m_TimerText = m_IGPanel.FindChild ("RemainingTime").GetComponent<Text> ();
+        m_ComboKill = 0;
+        m_ComboChest = 0;
+        ComboKillCoroutine = ComboKillTimer();
+        //ComboChestCoroutine = 
     }
 
     public void Init () {
     }
+
+    #region ComboKill
+    public void AddComboKill()
+    {
+        m_ComboKill++;
+        if(m_ComboKillCompteur==false)
+        {
+            m_ComboKillCompteur = true;
+            StartCoroutine(ComboKillCoroutine);
+        }
+        else
+        {
+            StopCoroutine(ComboKillCoroutine);
+            StartCoroutine(ComboKillCoroutine);
+        }
+    }
+
+    IEnumerator ComboKillTimer ()
+    {
+        yield return new WaitForSeconds(m_ComboKillTime);
+        m_ComboKillCompteur = false;
+        m_ComboKill = 0;
+    }
+    #endregion
+
+    #region ComboChest
+    public void AddComboChest()
+    {
+        m_ComboChest++;
+        if (m_ComboChestCompteur == false)
+        {
+            m_ComboChestCompteur = true;
+            StartCoroutine(ComboChestCoroutine);
+        }
+        else
+        {
+            StopCoroutine(ComboChestCoroutine);
+            StartCoroutine(ComboChestCoroutine);
+        }
+    }
+
+    IEnumerator ComboChestTimer()
+    {
+        yield return new WaitForSeconds(m_ComboChestTime);
+        m_ComboChestCompteur = false;
+        m_ComboChest = 0;
+    }
+    #endregion
 
     public void UpdateRemainingTime (float remainingTime) {
         if (m_TimerText == null) {
