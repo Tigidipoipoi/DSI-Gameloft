6b8d64be78e17afb42d2f9269bf00dbccf1dc5bb
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-03-15 14:37:14 +0100

    Adds EndCat

--- DSI-Gameloft/Assets/Scripts/Level/EndOfLevelScript.cs
+++ DSI-Gameloft/Assets/Scripts/Level/EndOfLevelScript.cs
@@ -2,5 +2,15 @@
 using System.Collections;
 
 public class EndOfLevelScript : MonoBehaviour {
-
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            if (FloorManager.instance.m_KeyAquired) {
+                // End of level
+                Debug.Log("End of level");
+            }
+            else {
+                Debug.Log("Gotta get the key !");
+            }
+        }
+    }
 }
