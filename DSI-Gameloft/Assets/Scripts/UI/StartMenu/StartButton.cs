d9a2ffefaa1d94261d40c1cace04ff3701d58c5f
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-03-16 18:06:12 +0100

    No more loading screen :'(

--- DSI-Gameloft/Assets/Scripts/UI/StartMenu/StartButton.cs
+++ DSI-Gameloft/Assets/Scripts/UI/StartMenu/StartButton.cs
@@ -7,22 +7,10 @@ public class StartButton : MonoBehaviour {
     public Transform m_InitialPosition;
 
     public bool m_Open;
-
-    int m_LoadProgress = 0;
     #endregion
 
     public void LaunchGame() {
         Application.LoadLevel("PlayTestV2");
-    }
-
-    IEnumerator DisplayLoadingScreen() {
-        // SetActive Loading Screen
-
-        AsyncOperation async = Application.LoadLevelAsync("PlayTestV2");
-        while (!async.isDone) {
-            m_LoadProgress = (int)(async.progress * 100.0f);
-            yield return null;
-        }
     }
 
     public void Exit() {
