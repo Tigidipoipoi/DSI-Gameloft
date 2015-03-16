f2df51f33a8728d7116e1e937b9c8fe1afa5c781
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-03-16 10:12:54 +0100

    Adds splashscreen

---
+++ DSI-Gameloft/Assets/Scripts/_Managers/SplashScreenScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenScript : MonoBehaviour {
+    void Start() {
+        PlayerPrefs.SetInt("RescuedCats", 0);
+        Application.LoadLevel("StartMenu");
+    }
+}
