8c41497330bf5b77707c63884608e28440433926
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-03-16 17:21:35 +0100

    Updates Enemies' colliders again

--- DSI-Gameloft/Assets/Scripts/UI/StartMenu/StartButton.cs
+++ DSI-Gameloft/Assets/Scripts/UI/StartMenu/StartButton.cs
@@ -7,23 +7,25 @@ public class StartButton : MonoBehaviour {
     public Transform m_InitialPosition;
 
     public bool m_Open;
+
+    int m_LoadProgress = 0;
     #endregion
 
-    public void LaunchGame () {
-        Application.LoadLevel ("PlayTestV2");
+    public void LaunchGame() {
+        Application.LoadLevel("PlayTestV2");
     }
 
     IEnumerator DisplayLoadingScreen() {
         // SetActive Loading Screen
 
-
-
-        yield return null;
+        AsyncOperation async = Application.LoadLevelAsync("PlayTestV2");
+        while (!async.isDone) {
+            m_LoadProgress = (int)(async.progress * 100.0f);
+            yield return null;
+        }
     }
 
-
-
-    public void Exit () {
-        Application.Quit ();
+    public void Exit() {
+        Application.Quit();
     }
 }
