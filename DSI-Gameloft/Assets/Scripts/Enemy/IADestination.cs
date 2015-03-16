c8b801284674ce8d5085e97a81b26061634c82f0
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-03-16 19:05:54 +0100

    WIP StandardEnemy Anim

--- DSI-Gameloft/Assets/Scripts/Enemy/IADestination.cs
+++ DSI-Gameloft/Assets/Scripts/Enemy/IADestination.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class IADestination : MonoBehaviour {
-
+    #region Members
     private Rigidbody m_Rigidbody;
 
     public Transform m_Destination_Cible;
@@ -19,82 +19,66 @@ public class IADestination : MonoBehaviour {
     private bool m_ChangeRotate;
     public float m_RotateSpeed;
 
-    void Start()
-    {
+    Animator m_Animator;
+    #endregion
+
+    void Start() {
         m_Rigidbody = this.gameObject.GetComponent<Rigidbody>();
 
-        if(m_Acceleration==0)
-        {
+        if (m_Acceleration == 0) {
             m_Acceleration = 0.1f;
             Debug.LogWarning("Pas d'acceleration mise. Acceleration tweaké automatiquement à 0.1");
         }
-        if(m_SpeedMax==0)
-        {
+        if (m_SpeedMax == 0) {
             Debug.LogWarning("Pas de speedmax tweaké!");
         }
-        if (m_ChangeRotate==true && m_RotateSpeed<=0)
-        {
+        if (m_ChangeRotate == true && m_RotateSpeed <= 0) {
             Debug.LogWarning("Pas de RotateSpeed tweaké");
-            if (m_SpeedMax == 0)
-            {
+            if (m_SpeedMax == 0) {
                 Debug.LogWarning("Pas de speedmax tweaké!");
             }
-                if (m_SpeedMax > 0)
-            {
+            if (m_SpeedMax > 0) {
                 Debug.LogWarning("Mise en place à la vitesse de speedmax");
             }
-           
         }
 
+        m_Animator = this.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (m_Destination_Cible != null)
-        {
-            if (Vector3.Distance(this.transform.position, m_Destination_Cible.position) <= m_BreakDistance)
-            {
+    void Update() {
+        if (m_Destination_Cible != null) {
+            if (Vector3.Distance(this.transform.position, m_Destination_Cible.position) <= m_BreakDistance) {
                 m_IsAtDistance = true;
+                m_Animator.SetBool("IsWalking", false);
                 m_Rigidbody.velocity = Vector3.zero;
-                if (m_RotateAroundPlayer == true)
-                {
-                    if (m_ChangeRotate)
-                    {
+                if (m_RotateAroundPlayer == true) {
+                    if (m_ChangeRotate) {
                         transform.RotateAround(m_Destination_Cible.position, Vector3.up, m_RotateSpeed * Time.deltaTime);
                     }
-                    else
-                    {
+                    else {
                         transform.RotateAround(m_Destination_Cible.position, Vector3.up, (m_RotateSpeed * Time.deltaTime) * -1);
                     }
-
                 }
             }
-            else
-            {
+            else {
                 m_IsAtDistance = false;
                 m_Direction = (m_Destination_Cible.position - this.gameObject.transform.position).normalized;
 
-                if (m_Speed < m_SpeedMax)
-                {
+                if (m_Speed < m_SpeedMax) {
                     m_Speed += m_Acceleration;
                 }
-                if (m_Speed > m_SpeedMax)
-                {
+                if (m_Speed > m_SpeedMax) {
                     m_Speed = m_SpeedMax;
                 }
+                m_Animator.SetBool("IsWalking", true);
                 m_Rigidbody.velocity = m_Direction * m_Speed;
             }
         }
-
     }
-    
-    void OnCollisionEnter(Collision other)
-    {
-        if(other.gameObject.layer==9)
-        {
+
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.layer == 9) {
             m_ChangeRotate = !m_ChangeRotate;
         }
     }
-    
 }
