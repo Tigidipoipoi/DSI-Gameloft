f155835998aab5867bb5a583d4292c4c81a4ea1d
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-03-18 01:06:14 +0100

    Adds EnnemyM anims

--- DSI-Gameloft/Assets/Scripts/Enemy/EnemyMissile/EnemyMissile.cs
+++ DSI-Gameloft/Assets/Scripts/Enemy/EnemyMissile/EnemyMissile.cs
@@ -1,89 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyMissile : Enemy_Script
-{
-
+public class EnemyMissile : Enemy_Script {
+    #region Members
     public IADestination m_Mouvement;
 
     private bool m_IsReady;
 
     public float m_DelayBeforeShoot;
-	public float m_DelayAfterShoot;
+    public float m_DelayAfterShoot;
 
 
     public GameObject m_PrefabBullet;
 
-    public  BulletScript m_BulletScript;
+    public BulletScript m_BulletScript;
     public float m_BulletPower;
     public float m_BulletSpeed;
 
     public Transform m_PointForShoot;
 
     public float m_RangeForShoot;
+    public Animator m_Animator;
+    #endregion
 
-
-	// Use this for initialization
-	public override void Start () 
-    {
+    public override void Start() {
         base.Start();
         m_IsReady = true;
         name = "EnemyMissile";
+    }
 
-        
-        
-
-	}
-
-	
-    IEnumerator WaitAndShoot()
-    {
-        if (m_IsAwake == true)
-        {
+    IEnumerator WaitAndShoot() {
+        if (m_IsAwake) {
             yield return new WaitForSeconds(m_DelayBeforeShoot);
-            if (m_Mouvement.m_IsAtDistance == false && (Vector3.Distance(this.transform.position, m_Mouvement.m_Destination_Cible.position) <= m_RangeForShoot))
-            {
+            if (m_Mouvement.m_IsAtDistance == false
+                && (Vector3.Distance(this.transform.position, m_Mouvement.m_Destination_Cible.position) <= m_RangeForShoot)) {
                 StopCoroutine(WaitAndShoot());
             }
-            GameObject bullet = Instantiate(m_PrefabBullet, m_PointForShoot.position, this.transform.rotation) as GameObject ;
-            m_BulletScript = bullet.GetComponent<BulletScript>();
-            m_BulletScript.m_BulletStats.m_Power = m_BulletPower;
-            m_BulletScript.m_BulletStats.m_Speed = m_BulletSpeed;
-            bullet.layer = LayerMask.NameToLayer("EnemyBullet");
 
-			yield return new WaitForSeconds(m_DelayAfterShoot);
-            m_IsReady = true;
+            m_Animator.SetTrigger("Shoot");
         }
     }
 
-	// Update is called once per frame
-	public override void Update () 
-    {
+    public IEnumerator Shoot() {
+        Debug.Log("Answer");
+        GameObject bullet = Instantiate(m_PrefabBullet, m_PointForShoot.position, this.transform.rotation) as GameObject;
+        m_BulletScript = bullet.GetComponent<BulletScript>();
+        m_BulletScript.m_BulletStats.m_Power = m_BulletPower;
+        m_BulletScript.m_BulletStats.m_Speed = m_BulletSpeed;
+        bullet.layer = LayerMask.NameToLayer("EnemyBullet");
+
+        yield return new WaitForSeconds(m_DelayAfterShoot);
+        m_Animator.ResetTrigger("Shoot");
+        m_IsReady = true;
+    }
+
+    public override void Update() {
         base.Update();
 
-        if (m_Mouvement.m_Destination_Cible == null)
-        {
+        if (m_Mouvement.m_Destination_Cible == null) {
             m_Mouvement.m_Destination_Cible = m_Player;
         }
 
-        if (m_IsAwake == true)
-        {
-            if ((m_Mouvement.m_IsAtDistance == true || (Vector3.Distance(this.transform.position,m_Mouvement.m_Destination_Cible.position)<=m_RangeForShoot))&& m_IsReady == true)
-            {
+        if (m_IsAwake) {
+            if ((m_Mouvement.m_IsAtDistance == true || (Vector3.Distance(this.transform.position, m_Mouvement.m_Destination_Cible.position) <= m_RangeForShoot)) && m_IsReady == true) {
                 m_IsReady = false;
 
                 StartCoroutine(WaitAndShoot());
-
             }
         }
 
-        if (m_IsFreeze == true)
-        {
+        if (m_IsFreeze) {
             m_Mouvement.enabled = false;
         }
-        else
-        {
+        else {
             m_Mouvement.enabled = true;
         }
-	}
+    }
 }
