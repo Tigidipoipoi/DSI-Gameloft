5303970c78db5e227a7b99d25951aae49fdf4566
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-03-18 18:25:24 +0100

    Integrates some RPs and updates weapon collection

--- DSI-Gameloft/Assets/Scripts/Loot/TimeBlock/TimeBlockScript.cs
+++ DSI-Gameloft/Assets/Scripts/Loot/TimeBlock/TimeBlockScript.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class TimeBlockScript : MonoBehaviour {
-
+    #region Members
     public float m_MagnetizeDistance;
     private float m_DistanceFromPlayer;
     private Rigidbody m_Rigidbody;
@@ -17,10 +17,10 @@ public class TimeBlockScript : MonoBehaviour {
     public float m_GainTime;
 
     AudioSource m_AudioSource;
-    bool m_IsCatch;
+    bool m_IsCatch; 
+    #endregion
 
-    void Start()
-    {
+    void Start() {
         m_AudioSource = GetComponent<AudioSource>();
 
         m_Rigidbody = GetComponent<Rigidbody>();
@@ -28,38 +28,29 @@ public class TimeBlockScript : MonoBehaviour {
         m_IsCatch = false;
     }
 
-	// Update is called once per frame
-	void Update () 
-    {
-	    if(m_Player!=null)
-        {
-            
-            m_EcartDistance=  Vector3.Distance(this.transform.position, m_Player.position);
+    void Update() {
+        if (m_Player != null) {
+            m_EcartDistance = Vector3.Distance(this.transform.position, m_Player.position);
 
-            if (m_EcartDistance < m_MagnetizeDistance)
-            {
+            if (m_EcartDistance < m_MagnetizeDistance) {
                 m_Direction = (m_Player.position - this.gameObject.transform.position).normalized;
 
-                m_Speed = Mathf.Abs((m_MagnetizeDistance - m_EcartDistance))*4;
+                m_Speed = Mathf.Abs((m_MagnetizeDistance - m_EcartDistance)) * 4;
                 m_Rigidbody.velocity = m_Direction * m_Speed;
             }
         }
 
-        if (!m_AudioSource.isPlaying && m_IsCatch==true)
-        {
+        if (!m_AudioSource.isPlaying && m_IsCatch == true) {
             Destroy(this.gameObject);
         }
-	}
+    }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag=="Player")
-        {
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
             m_AudioSource.Play();
             TimerManager.instance.AddTime(m_GainTime);
             m_IsCatch = true;
             Destroy(this.gameObject);
         }
     }
-
 }
