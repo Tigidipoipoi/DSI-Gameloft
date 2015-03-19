8471813ce99afcc40235e9e78ed0e82cbb9a0f9f
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-03-19 17:12:12 +0100

    WIP Integration StartMenu

---
+++ DSI-Gameloft/Assets/Scripts/ActivateAnimators.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateAnimators : MonoBehaviour {
+    #region Members
+    public Animator m_UIAchievementsAnimator;
+    public Animator m_UICreditsAnimator;
+    public Animator m_UIEndLevelAnimator;
+    public Animator m_UILeaderboardAnimator;
+    public Animator m_UILevelSelectAnimator;
+    public Animator m_UPlayscreenAnimator;
+    public Animator m_UISettingsAnimator;
+    public Animator m_UIShopAnimator;
+    #endregion
+
+    void Start() {
+        this.StartCoroutine("ActivateAnimator");
+    }
+
+    IEnumerator ActivateAnimator() {
+        yield return null;
+
+
+    }
+}
